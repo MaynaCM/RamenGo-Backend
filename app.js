8d706ae5340c3a9076e64c928b471765f1e81fe9
MaynaCM <maynnamartins@gmail.com>
2024-06-02 22:53:40 -0300

    cors require added to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const apiKeyMiddleware = require('./middleware/apiKeyMiddleware');
 const app = express();
 const port = process.env.PORT || 3000;
