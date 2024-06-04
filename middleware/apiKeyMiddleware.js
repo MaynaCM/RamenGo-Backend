c1a2d80d29e508335b8f87a4abb2e8781b3a11b2
MaynaCM <maynnamartins@gmail.com>
2024-06-04 00:32:06 -0300

    valid api key test

--- middleware/apiKeyMiddleware.js
+++ middleware/apiKeyMiddleware.js
@@ -8,6 +8,7 @@ const apiKeyMiddleware = (req, res, next) => {
 
     const apiKey = req.header('x-api-key');
     const validApiKey = process.env.API_KEY;
+    console.log(validApiKey)
 
     if (!apiKey || apiKey !== validApiKey) {
         return res.status(403).json({ error: 'Forbidden: x-api-key header missing' });
